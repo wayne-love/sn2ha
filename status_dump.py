57c9a1016f17b2d34cdb762efdfab1b53869ea29
Wayne Love <wayne@gsjs.org>
2021-10-03 09:33:05 +1100

    Remove debuggind code and static ip

--- status_dump.py
+++ status_dump.py
@@ -4,7 +4,7 @@ import time
 socket.setdefaulttimeout(5)
 s = socket.socket()        
 
-s.connect(('172.16.0.5', 2000))
+s.connect(('WiFly-EZX', 2000))
 print (s.recv(1024))
 time.sleep(0.5)
 s.send('\n'.encode())
@@ -14,8 +14,7 @@ response = s.recv(1024).split(b",")
 
 x = 0
 for value in response:
-    if len(value) == 3:
-        print(x,value)
+    print(x,value)
     x = x + 1
 
 s.close                     # Close the socket when done
